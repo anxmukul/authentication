fc2bb3ea93ad488a456f5e842bc3bf1b1aeb8855
anxmukul <kmukul1197@gmail.com>
2023-09-26 13:05:13 +0530

    Frontend initial setup

---
+++ src/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Registration.css'
+
+function Registration(){
+  return (
+    <div className='Form'>
+      <h3>Registration Form</h3>
+    </div>
+  )
+}
+
+export default Registration
