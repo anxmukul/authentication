9d9766fa8f29c8452ef62ef7f7c33e2855dccb7e
anxmukul <kmukul1197@gmail.com>
2023-09-26 17:11:47 +0530

    Installing CORS

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const { Client } = require('pg');
-const port = 3001;
-
+const cors = require("cors")
 app.use(express.json());
+app.use(cors());
+
+const port = 3001;
 
 app.get("/", (req, res) => {
     res.send("This is a express server");
